9baeac21034817761df57443142d2b3cfd7c4bf7
Martin Pokorny <mpokorny@nrao.edu>
2020-01-30 08:52:59 -0700

    Patch from Make Bauer, backported to stable

--- runtime/legion/runtime.cc
+++ runtime/legion/runtime.cc
@@ -621,7 +621,7 @@ namespace Legion {
       }
       if (subscription_event.exists())
       {
-        Runtime::trigger_event(subscription_event);
+        Runtime::trigger_event(subscription_event, future_complete);
         subscription_event = ApUserEvent::NO_AP_USER_EVENT;
         if (remove_base_resource_ref(RUNTIME_REF))
           assert(false); // should always hold a reference from caller
